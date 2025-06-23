b7a70949a6240bd7d65d270c742215eae11bd62a
yahyaelganayni1 <yahyaelganayni@gmail.com>
2025-06-23 13:33:56 +0300

    refactor: Remove commented-out debug code from generate_feedback method

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -26,12 +26,7 @@ class Guess < ApplicationRecord
   def generate_feedback
     guess_code = code.split('')
     secret_code = game.secret_code.split('')
-    # p "Guess code: A111#{guess_code}"
-    # p "Secret code: A111#{secret_code}"
-    # exact_matches = guess_code.each_with_index.count { |number, i| number == secret_code[i] }
-    # number_matches = (guess_code & secret_code).flat_map { |number| [guess_code.count(number), secret_code.count(number)].min }.sum - exact_matches
-    # p "Exact: #{exact_matches}, number only: #{number_matches}"
-    # self.feedback = "Exact: #{exact_matches}, number only: #{number_matches}"
+
     feedback = []
     guess_code.each_with_index do |number, i|
       if number == secret_code[i]
