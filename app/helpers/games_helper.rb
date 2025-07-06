d2fa378f9b6b9ba11595523ece663b36162b1896
yahyaelganayni1 <yahyaelganayni@gmail.com>
2025-07-06 09:46:11 +0300

    fix: Simplify share messages by removing score references

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,6 +1,6 @@
 module GamesHelper
   def share_on_facebook(game)
-    share_text = "I just cracked the code in #{game.attempts} attempts and scored #{game.score} points! 🎉 Can you beat my score?"
+    share_text = "I just cracked the code in #{game.attempts} attempts! 🎉 Can you beat me?"
     share_url = game_url(game)
 
     facebook_url = "https://www.facebook.com/sharer/sharer.php"
@@ -13,7 +13,7 @@ module GamesHelper
   end
 
   def share_on_twitter(game)
-    share_text = "I just cracked the code in #{game.attempts} attempts and scored #{game.score} points! 🎉 Can you beat my score? #BreakTheCode"
+    share_text = "I just cracked the code in #{game.attempts} attempts! 🎉 Can you beat me? #BreakTheCode"
     share_url = game_url(game)
 
     twitter_url = "https://twitter.com/intent/tweet"
@@ -26,6 +26,6 @@ module GamesHelper
   end
 
   def share_message(game)
-    "I just cracked the code in #{game.attempts} attempts and scored #{game.score} points! 🎉 Can you beat my score? Play Break the Code: #{game_url(game)}"
+    "I just cracked the code in #{game.attempts} attempts! 🎉 Can you beat me? Play Break the Code: #{game_url(game)}"
   end
 end
